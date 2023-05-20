718ec301498e044ac876a90f24e22c2bf8d6a7b8
katsmunoz <katherine.smm@gmail.com>
2023-05-20 19:11:31 -0400

    list of items

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  products: Product[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ionViewDidEnter() {
+    this.apiService.getProducts().subscribe((response: Product[]) => {
+      this.products = response;
+    });
+  }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    const url = `${environment.apiUrl}/products`;
+    return this.http.get<Product[]>(url);
+  }
+}
